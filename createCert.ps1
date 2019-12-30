a68320fe11339d33a5a38bc32ea12fa47206b6dd
Automate4aBetterLife <ratanm@live.ca>
2019-12-30 17:28:07 -0500

    Update createCert.ps1

--- createCert.ps1
+++ createCert.ps1
@@ -1,3 +1,27 @@
+Function pickFile(){
+    <#
+    .SYNOPSIS
+        Pick a file from PowerShell.
+    .DESCRIPTION
+        Ask user to pick a file in a script
+    .PARAMETER User
+        $initialDirectory start from user's Desktop  
+    .EXAMPLE
+        pickFile
+    .NOTES
+        Author: Ratan Mohapatra
+        Last Update: December 30, 2019 
+    #>    
+        $initialDirectory = "$env:USERPROFILE\desktop"
+        [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+        $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+        $OpenFileDialog.initialDirectory = $initialDirectory
+        $OpenFileDialog.filter = "PS1 (*.ps1)| *.ps1"
+        $OpenFileDialog.ShowDialog() | Out-Null
+        return $OpenFileDialog.filename
+        }
+
 function MakeMySignCert{
     <#
     .SYNOPSIS
@@ -6,11 +30,12 @@ function MakeMySignCert{
         Codesigning a script allows you white list the CodeSigning Certificate and to run the script on remote machines
         Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine -Force
     .PARAMETER User
-        $yourEmail is stroed in the CN field of certificate   
+        $yourEmail is stored in the CN field of certificate   
     .EXAMPLE
         MakeMySignCert -yourEmail 'me@myemail.edu'
     .NOTES
-        Author: Ratan Mohapatra | https://www.linkedin.com/in/ratanm/
+        Author: Ratan Mohapatra
+        https://github.com/ratanGit/code-sign-PS
         Last Update: December 30, 2019 
     #>
     [CmdletBinding()]
@@ -31,4 +56,25 @@ function MakeMySignCert{
     #list certs in my cert dir
     write-host -BackgroundColor Green -ForegroundColor White "`nYou have the following Code Signing Certs available!"
     dir cert:\currentuser\my -CodeSigningCert
+    }
+
+Write-Host -BackgroundColor Green -ForegroundColor White "`nRatan's CodeSigning Script 2020`n"
+
+$chk = [int](Read-Host "Select your options (e.g., 2): `n1`tWant to create a new CodeSigning Certificate `n2`tWant to sign a script`n`t")
+
+
+switch($chk){
+    1{
+      $email = Read-Host 'What is your email : e.g., me@myemail.edu '
+      MakeMySignCert -yourEmail $email
+      }
+    2{
+      $fl = pickfile
+      #$cert = @(Get-ChildItem cert:\CurrentUser\My -codesigning)[1]
+      $cert = @(Get-ChildItem cert:\CurrentUser\My -codesigning) ; $cert
+      $WhichCert = Read-Host 'Please select the Certificate you want to use, e.g., 0,1,2,... '
+      $UseCert = $cert[$WhichCert]
+      Set-AuthenticodeSignature -FilePath $fl -Certificate $UseCert
+      }
+    default{end}
     }
